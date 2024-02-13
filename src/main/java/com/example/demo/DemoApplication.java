1e734afb9345e3e202ffc5783b3fa3677b1fce2b
santhoshbabu0411 <96950191+santhoshbabu0411@users.noreply.github.com>
2024-02-13 16:30:23 +0530

    Update DemoApplication.java
    Title changed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,8 +13,7 @@ public class DemoApplication {
 
 	@GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
+        model.addAttribute("title", "Hello this is my first spring-boot project");
         return "index";
     }
 
